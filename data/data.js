ef61f6c90c8128e7ac3fb6396911adab61dbe1ef
momen885 <momen885@yahoo.com>
2022-01-22 01:12:39 +0200

    collecting some data and setting up home page

---
+++ data/data.js
@@ -0,0 +1,14 @@
+const mobiles = [
+  {
+    category: "Apple",
+    id: 1,
+    name: "iPhone-12",
+    img: "iphone-12.jpg",
+  },
+  {
+    category: "Apple",
+    id: 2,
+    name: "iPhone-12-mini",
+    img: "iphone-12-mini.jpg",
+  },
+];
